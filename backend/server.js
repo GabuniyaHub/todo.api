dd56f8b07266dff5b2777e08d908b633be64a46c
GabuniyaHub <dianka.gabuniya@gmail.com>
2026-07-24 20:18:02 +0500

    update reamdMd

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import url from 'node:url';
 
+const __Filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__Filename);
+const frontendDir = path.join(__dirname, '..', 'frontend');
+const indexPath = path.join(frontendDir, 'index.html');
+
 http.createServer( async (request, response) => {
-    
-    const __Filename = url.fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__Filename);
-    const frontendDir = path.join(__dirname, '..', 'frontend');
-    const indexPath = path.join(frontendDir, 'index.html');
     
     if (request.method === 'GET' && request.url === '/') {
         try {
@@ -20,7 +20,42 @@ http.createServer( async (request, response) => {
         } catch (err) {
             console.error(err)
             response.writeHead(500);
-            response.end('404 ошибка')
+            response.end('Сервер недоступен. Повтрите попытку позже')
+        }
+    }
+
+    if (request.method === 'GET' && request.url === "/api/todos") {
+        try {
+            const data = {
+                message: "hello from node js API!",
+                todos: [
+                    { id: 1, title: 'Изучить node js', completed: false },
+                    { id: 2, title: 'Сделать Todo API', completed: false },
+                ] 
+            };
+
+            const jsonBody = JSON.stringify(data);
+
+            response
+                .writeHead( 200, {
+                    'Content-Length': Buffer.byteLength(jsonBody),
+                    'Content-Type': 'application/json',
+                })
+                .end(jsonBody);
+        } catch ( err ){
+            console.error(err)
+            response.writeHead(500);
+            responce.end('Сервер недоступен. Повтрите попытку позже')
+        }
+    }
+
+    if (request.method === 'POST' && request.url === "/api/todos") {
+        try {
+
+        } catch ( err ){
+            console.error(err)
+            response.writeHead(500);
+            responce.end('Сервер недоступен. Повтрите попытку позже')
         }
     }
 }).listen(3000, "127.0.0.1", () => {
